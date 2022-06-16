d3c328f9d20e1bf7c7e42dc406db24bf04dbdcef
Boris <Lopez98boris@gmail.com>
2022-06-16 14:20:54 -0400

    first commit Google analyticis

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import{Router, NavigationEnd} from '@angular/router';
+
+//declare gives Angular app access to ga function
+declare let gtag: Function;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularGoogleAnalytics';
+
+  constructor(public router: Router){
+
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+
+        console.log(event.urlAfterRedirects);
+        gtag('config', 'G-P61F7JWBGT', {'page_path': event.urlAfterRedirects});
+      }
+    })
+  }
+}
